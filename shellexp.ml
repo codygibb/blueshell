f00f54d4d1844173d6f1e6deab405fe09d59d35a
Cody Gibb <codyhgibb@gmail.com>
2016-03-03 15:56:39 -0800

    Fixes to shellcall experiment
    - TODO: get last working dir of exiting process

--- shellexp.ml
+++ shellexp.ml
@@ -1,14 +1,19 @@
 open Printf
 open Core.Std
 
+let print_fd fd = fd |> Unix.in_channel_of_descr |> In_channel.input_all |> print_string
+
 let () =
-  let cmd = "ls | grep '^.*ml$'" in
-  let pinfo = Unix.create_process ~prog:"/bin/sh" ~args:["-c"; sprintf "cd %s && %s" (Unix.getcwd ()) cmd] in
+  let dir = Filename.concat (Unix.getcwd ()) (Filename.concat "test" "progs") in
+  let cmd = "ls -A1 -R | grep '^.*blu$'" in
+  Unix.chdir dir;
+  let pinfo = Unix.create_process ~prog:"/bin/sh" ~args:["-c"; cmd] in
   let open Unix.Process_info in
-  let ic = Unix.in_channel_of_descr pinfo.stdout in
-  let output = In_channel.input_all ic in
   (match Unix.waitpid pinfo.pid with
-  | Ok () -> ()
-  | Error (`Exit_non_zero i) -> printf "exit non-zero: %d\n" i
-  | Error (`Signal signal) -> printf "exit signal: %s\n" (Signal.to_string signal));
-  print_endline output
+  | Ok () ->  print_fd pinfo.stdout 
+  | Error (`Exit_non_zero i) ->
+      printf "exit non-zero: %d\n" i;
+      print_fd pinfo.stderr
+  | Error (`Signal signal) ->
+      printf "exit signal: %s\n" (Signal.to_string signal);
+      print_fd pinfo.stderr);
