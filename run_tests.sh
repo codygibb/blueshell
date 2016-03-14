828819fc9bc24df75b7f38400580982273ba2d0c
Cody Gibb <codyhgibb@gmail.com>
2016-03-14 02:55:43 -0700

    Moved everything to "src/"

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,11 @@
 echo ">>> Unit tests <<<"
 echo
-ocamlbuild -use-ocamlfind test/unit/tests.byte || { echo 'Build failed'; exit 1; }
+ocamlbuild -I src -use-ocamlfind test/unit/tests.byte || { echo 'Build failed'; exit 1; }
 ./tests.byte
 rm tests.byte
 echo
 echo ">>> Integration tests <<<"
 echo
-ocamlbuild -use-ocamlfind test/integration/tests.byte || { echo 'Build failed'; exit 1; }
+ocamlbuild -I src -use-ocamlfind test/integration/tests.byte || { echo 'Build failed'; exit 1; }
 ./tests.byte test/integration/progs
 rm tests.byte
